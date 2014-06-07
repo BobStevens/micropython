b764520c5084426221cb8b8f228f4f9fd9229dbe
BobStevens <rpstevens@gmail.com>
2014-06-07 18:29:36 -0400

    Update BMP085.py
    readTemperature bug

--- BMP085/BMP085.py
+++ BMP085/BMP085.py
@@ -151,7 +151,7 @@ class BMP085 :
     UT = self.readRawTemp()
     X1 = ((UT - self._cal_AC6) * self._cal_AC5) >> 15
     X2 = (self._cal_MC << 11) / (X1 + self._cal_MD)
-    B5 = X1 + X2
+    B5 = int(X1 + X2)
     temp = ((B5 + 8) >> 4) / 10.0
     if (self.debug):
       print("DBG: Calibrated temperature = %f C" % temp)
